0008f9ef3c709a2e2f57790001757e8d3fd29671
Eduardo Cabral <eduardomcabral1974@gmail.com>
2022-12-15 21:07:11 -0300

    dando forma

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AcercaDeMiComponent } from './componentes/acerca-de-mi/acerca-de-mi.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { SkillsComponent } from './componentes/skills/skills.component';
+import { ContactoComponent } from './componentes/contacto/contacto.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AcercaDeMiComponent,
+    NavbarComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    SkillsComponent,
+    ContactoComponent,
+    FooterComponent,
+    ProyectosComponent,
   ],
   imports: [
     BrowserModule
